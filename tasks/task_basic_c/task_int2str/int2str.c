2c850bb790f3565eadbc63a80766c810e8de3767
Evg1016 <edahno199@gmail.com>
2024-10-08 03:19:58 +0300

    Программа проходит все тесты

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -2,7 +2,24 @@
 #include "int2str.h"
 #include "stdio.h"
 
-char* int2str(int number) {
-    char* str = "0";
-    return str;
+int str2int(const char *str) {
+  int znak = 1;
+  int result = 0;
+
+  if (*str == '-'){
+    znak = -1;
+    str++;
+  } else if (*str == '+') {
+    str++;
+  }
+
+  int count = 0;
+  while (*str != '\0') {
+    result = result * 10 + (*str - '0');
+    count += 1;
+    str++;
+    assert(znak == -1 && result == -2147483648 || znak == -1 && result >= 0 && count <= 9 || znak == 1 && result >= 0);
+  }
+  assert(count > 0);
+  return result * znak;
 }
