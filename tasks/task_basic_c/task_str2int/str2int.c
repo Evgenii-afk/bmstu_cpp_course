f225669d0c6452ef3b30c764829b40662a8e9f48
Evg1016 <edahno199@gmail.com>
2024-10-08 04:17:00 +0300

    Моя попытка номер 5

--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -1,42 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <ctype.h>
-#include <string.h>
+#include <assert.h>
+#include "int2str.h"
+#include "stdio.h"
 
+int str2int(const char *str) {
+  int znak = 1; 
+  int result = 0;
 
+  if (*str == '-'){
+    znak = -1;
+    str++;
+  } else if (*str == '+') {
+    str++;
+  }
 
-int str2int(const char *str){
-    int number = 0;
-    int step = 1;
-    for (int i=strlen(str)-2; i>=0; i--){
-        switch(str[i]){
-            case '0': number+=0*step; break;
-            case '1': number+=1*step; break;
-            case '2': number+=2*step; break;
-            case '3': number+=3*step; break;
-            case '4': number+=4*step; break;
-            case '5': number+=5*step; break;
-            case '6': number+=6*step; break;
-            case '7': number+=7*step; break;
-            case '8': number+=8*step; break;
-            case '9': number+=9*step; break;
-        }
-        step*=10;
-    }
-    return number;
-    
-    return 0;
-}
-
-int main(void){
-    char line[256]={0};
-    fgets(line, 256, stdin);
-    for (int i=0; i<strlen(line)==1; i++){
-        if (isalpha(line[i])){
-            printf("есть буква");
-            break;
-        }
-    }int number = 0;
-    printf ("%d\n", str2int(line));
+  int count = 0;
+  while (*str != '\0') {
+    result = result * 10 + (*str - '0');
+    count += 1;
+    str++;
+    assert(znak == -1 && result == -2147483648 || znak == -1 && result >= 0 && count <= 9 || znak == 1 && result >= 0);
+  }
+  assert(count > 0);
+  return result * znak;
 }
