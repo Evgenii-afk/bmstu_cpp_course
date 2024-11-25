59acd8f5c4b131fed496b0ab4956ebeb9904090b
Evgeny <edahno199@gmail.com>
2024-11-26 00:07:26 +0300

    проходит style check

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,33 +1,28 @@
 #include <assert.h>
+#include <malloc.h>
 #include "int2str.h"
 #include "stdio.h"
-#include <malloc.h>
 
-char* int2str(int number) { 
-    char *str = (char*)malloc(12 * sizeof(char));
+char* int2str(int number) {
+    char* str = malloc(12 * sizeof(char));
 
-    unsigned int num = (unsigned int)((number < 0) * -number) + (number >= 0) * number; 
+    unsigned int num = (unsigned int)((number < 0) * -number) + (number >= 0) * number;
     int j = 12;
 
-    str[12] = '\0'; // Конец строки
-
+    str[12] = '\0';  // Конец строки
     if (num == 0) {
         str[j] = '0';
-        return str + j; // Возвращаем указатель на строку
+        return str + j;  // Возвращаем указатель на строку
     }
-
-    
-
     while (num > 0) {
-        str[j] = (num % 10) + '0'; // Запись символа
+        str[j] = (num % 10) + '0';  // Запись символа
         num /= 10;
         j--;
     }
-
     if (number < 0) {
         str[j] = '-';
         j--;  // Сдвигаем индекс для знака
     }
 
-    return str + j + 1; // Возвращаем указатель на строку
+    return str + j + 1;  // Возвращаем указатель на строку
 }
--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -8,14 +8,16 @@ int str2int(const char *str) {
   int result = 0;
   int sign = 1;
   int lenght = 0;
-  if (*str == '+') {str++;}
-  else if (*str == '-') {sign = -1;
-    str++;}
-
+  if (*str == '+') {
+    str++;
+  } else if (*str == '-') {
+    sign = -1;
+    str++;
+    }
   while ((*str >= '0' && *str <= '9') && *str != '\0') {
     int number = *str - '0';
     if (sign == 1 && (result > (int_rng_mx - number) / 10)) {assert(1 == 0);}
-    if (sign == -1 && (result > (int_rng_mn -1 - number ) / 10)) {assert(1 == 0);}
+    if (sign == -1 && (result > (int_rng_mn -1 - number) / 10)) {assert(1 == 0);}
     result = result * 10 + number;
     str++;
     lenght++;
