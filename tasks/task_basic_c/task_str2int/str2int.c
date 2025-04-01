483da82f281ada6af5e9ed68247b4dcb5eb23c35
Evgeny <edahno199@gmail.com>
2025-04-01 23:32:29 +0300

    str2int

--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -4,5 +4,26 @@
 
 int str2int(const char* str)
 {
-	return 0;
+	int len=0;
+	int is_negative=0;
+	int start=0;
+	int res=0;
+
+	while(str[len]!='\0'){
+		len++;
+	}
+
+	if(str[0]=='-'){
+		is_negative=1;
+		start++;
+	}else if(str[0]=='+'){
+		start++;
+	}
+
+	while(str[start]!='\0'){
+		res+=res*10 + str[len]+'0';
+	}
+
+	return res;
+
 }
