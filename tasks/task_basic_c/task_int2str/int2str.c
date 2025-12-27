7e2c39e53f23b4d1ae93e5787ef2d733c7aae4ba
po4k <edahno199@gmail.com>
2025-12-27 08:51:25 +0300

    task_basic_c rewrite

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,9 +1,65 @@
 #include "int2str.h"
-#include <assert.h>
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 char* int2str(int number)
 {
-	char* str = "0";
+	char* str = (char*)malloc(12 * sizeof(char));
+
+	if (str == NULL)
+	{
+		return NULL;
+	}
+
+	int i = 0;
+	int is_negative = 0;
+
+	if (number == -2147483648)
+	{
+		char* min_int = "-2147483648";
+		for (int j = 0; j <= 11; j++)
+		{
+			str[j] = min_int[j];
+		}
+		return str;
+	}
+
+	if (number == 0)
+	{
+		str[0] = '0';
+		str[1] = '\0';
+		return str;
+	}
+
+	if (number < 0)
+	{
+		is_negative = 1;
+		number = -number;
+	}
+
+	while (number > 0)
+	{
+		str[i++] = (number % 10) + '0';
+		number /= 10;
+	}
+
+	if (is_negative)
+	{
+		str[i++] = '-';
+	}
+
+	str[i] = '\0';
+
+	int start = 0;
+	int end = i - 1;
+	while (start < end)
+	{
+		char temp = str[start];
+		str[start] = str[end];
+		str[end] = temp;
+		start++;
+		end--;
+	}
+
 	return str;
 }
--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -1,8 +1,65 @@
 #include "str2int.h"
-#include <assert.h>
-#include "stdio.h"
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int str2int(const char* str)
 {
-	return 0;
+	if (str == NULL || *str == '\0')
+	{
+		abort();
+	}
+
+	int sign = 1;
+	long long result = 0;
+	int i = 0;
+
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (str[i] == '+')
+	{
+		i++;
+	}
+
+	if (str[i] == '\0')
+	{
+		abort();
+	}
+
+	while (str[i] != '\0')
+	{
+		if (str[i] < '0' || str[i] > '9')
+		{
+			abort();
+		}
+
+		int digit = str[i] - '0';
+
+		if (result > (LLONG_MAX - digit) / 10)
+		{
+			abort();
+		}
+
+		result = result * 10 + digit;
+
+		if ((sign == 1 && result > INT_MAX) ||
+			(sign == -1 && result > (long long)INT_MAX + 1))
+		{
+			abort();
+		}
+
+		i++;
+	}
+
+	result *= sign;
+
+	if (result < INT_MIN || result > INT_MAX)
+	{
+		abort();
+	}
+
+	return (int)result;
 }
